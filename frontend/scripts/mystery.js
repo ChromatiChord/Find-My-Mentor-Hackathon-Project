f6b3cd8c0b3a3660a8c0ca3a3dd0cc1d5952443d
ChromatiChord <callumward56@gmail.com>
2021-07-18 02:40:37 +1000

    changed colour scheme

--- frontend/scripts/mystery.js
+++ frontend/scripts/mystery.js
@@ -1,5 +1,6 @@
 function main() {
     var message = document.createElement("h1");
+    document.getElementById("spooky_sound").volume = 0.2;
     message.innerHTML = "I told you not to click the link";
     message.className = "mystery_message";
     var value = 100;
@@ -13,7 +14,8 @@ function main() {
             document.body.appendChild(message);
         }
         if (value < 0) {
-            document.body.style = "background-color: orange";
+            document.body.style = "background-color: coral";
+            document.getElementById("spooky_sound").volume = 0;
         }
     });
     document.body.appendChild(button);
